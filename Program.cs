e0e961cfab5b1518c51965574aac67b3b16f5de3
shiva-12345 <munnoorushiva07@gmail.com>
2021-03-28 19:14:28 +0530

    main program file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        public static void Main()
+        {
+            EmpWageBuilderObject DMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            DMart.computeEmpWage();
+            EmpWageBuilderObject Relaince = new EmpWageBuilderObject("Relaince", 10, 4, 20);
+            Relaince.computeEmpWage();
+        }
+    }
+}
