4bc18297476c14b2845f3b72e5585aa59b3de9a0
shiva-12345 <munnoorushiva07@gmail.com>
2021-03-28 20:19:09 +0530

    main program

---
+++ InterfaceApproach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InterfaceApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for DMart company: "+empWageBuilder.getTotalWage("DMart"));
+
+
+        }
+    }
+}
